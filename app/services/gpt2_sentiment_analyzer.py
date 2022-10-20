8b567c251dda80625be3c1c3f82852fc34e0e3ae
Dean <dean.c.garcia@gmail.com>
2022-10-20 13:28:04 -0700

    Update gpt2_sentiment_analyzer.py

--- app/services/gpt2_sentiment_analyzer.py
+++ app/services/gpt2_sentiment_analyzer.py
@@ -19,33 +19,33 @@ tokenizer = GPT2Tokenizer.from_pretrained(pretrained_model_name_or_path=model_na
 # default to left padding
 tokenizer.padding_side = "left"
 # Define PAD Token = EOS Token = 50256
-#tokenizer.pad_token = tokenizer.eos_token
-#model = GPT2ForSequenceClassification.from_pretrained(pretrained_model_name_or_path="\home\deila_weights.pth", config=model_config)
+tokenizer.pad_token = tokenizer.eos_token
+model = GPT2ForSequenceClassification.from_pretrained(pretrained_model_name_or_path="\home\deila_weights.pth", config=model_config)
 # resize model embedding to match new tokenizer
-#model.resize_token_embeddings(len(tokenizer))
+model.resize_token_embeddings(len(tokenizer))
 
 # fix model padding token id
-#model.config.pad_token_id = model.config.eos_token_id
-#model.to(device)
+model.config.pad_token_id = model.config.eos_token_id
+model.to(device)
 
 ## Only do the static app and as the next step want to integrate the local app. 
 def model_classify(zero, init_token):
     #let it train here show it in demo
-    #global tokenizer
-    #global model
-    #model.eval()
-    #init_id = tokenizer.encode(init_token)
-    #result = init_id
-    #init_input = torch.tensor(init_id).unsqueeze(zero).to(device)
+    global tokenizer
+    global model
+    model.eval()
+    init_id = tokenizer.encode(init_token)
+    result = init_id
+    init_input = torch.tensor(init_id).unsqueeze(zero).to(device)
 
-    #with torch.no_grad():
-        #output = model(init_input)
-        #logits = output.logits[:2]
-        #logits = logits.detach().cpu().numpy()
-        #print(logits)
-        #predict_content = logits.argmax(axis=-1).flatten().tolist()
-        #print(predict_content)
+    with torch.no_grad():
+        output = model(init_input)
+        logits = output.logits[:2]
+        logits = logits.detach().cpu().numpy()
+        print(logits)
+        predict_content = logits.argmax(axis=-1).flatten().tolist()
+        print(predict_content)
 
-    return {0,1}
+    return logits
 
 print("Model Loaded")
