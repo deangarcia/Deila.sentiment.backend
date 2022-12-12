9a7c2360b6d6cac8c673f35aa95b1e1320afc0b2
Dean <dean.c.garcia@gmail.com>
2022-12-11 16:38:46 -0800

    should work here now

--- app/services/gpt2_sentiment_analyzer.py
+++ app/services/gpt2_sentiment_analyzer.py
@@ -5,44 +5,44 @@ import torch
 import torchvision.models as models
 from transformers import (GPT2Config, GPT2Tokenizer, GPT2ForSequenceClassification)
 
-#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-#model_name_or_path = 'gpt2'
-#labels_ids = {'neg': 0, 'pos': 1}
-#n_labels = len(labels_ids)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model_name_or_path = 'gpt2'
+labels_ids = {'neg': 0, 'pos': 1}
+n_labels = len(labels_ids)
 
-#model_config = GPT2Config.from_pretrained(pretrained_model_name_or_path=model_name_or_path, num_labels=n_labels)
+model_config = GPT2Config.from_pretrained(pretrained_model_name_or_path=model_name_or_path, num_labels=n_labels)
 ## Get model's tokenizer.
-#print('Loading tokenizer...')
+print('Loading tokenizer...')
 ## load the saved model here
-#tokenizer = GPT2Tokenizer.from_pretrained(pretrained_model_name_or_path=model_name_or_path)
+tokenizer = GPT2Tokenizer.from_pretrained(pretrained_model_name_or_path=model_name_or_path)
 ## default to left padding
-#tokenizer.padding_side = "left"
+tokenizer.padding_side = "left"
 
 ## Define PAD Token = EOS Token = 50256
-#tokenizer.pad_token = tokenizer.eos_token
-#model = GPT2ForSequenceClassification.from_pretrained(pretrained_model_name_or_path="/home/deila_weights.pth", config=model_config)
+tokenizer.pad_token = tokenizer.eos_token
+model = GPT2ForSequenceClassification.from_pretrained(pretrained_model_name_or_path="/home/deila_weights.pth", config=model_config)
 
 ## resize model embedding to match new tokenizer
-#model.resize_token_embeddings(len(tokenizer))
+model.resize_token_embeddings(len(tokenizer))
 
 ## fix model padding token id
-#model.config.pad_token_id = model.config.eos_token_id
-#model.to(device)
+model.config.pad_token_id = model.config.eos_token_id
+model.to(device)
 
 ## Only do the static app and as the next step want to integrate the local app. 
 def model_classify(zero, init_token):
-    #global tokenizer
-    #global model
-    #model.eval()
-    #init_id = tokenizer.encode(init_token)
-    #result = init_id
-    #init_input = torch.tensor(init_id).unsqueeze(zero).to(device)
+    global tokenizer
+    global model
+    model.eval()
+    init_id = tokenizer.encode(init_token)
+    result = init_id
+    init_input = torch.tensor(init_id).unsqueeze(zero).to(device)
 
-    #with torch.no_grad():
-        #output = model(init_input)
-        #logits = output.logits[:2]
-        #logits = logits.detach().cpu().numpy()
-        #predict_content = logits.argmax(axis=-1).flatten().tolist()
-    return {0,1}#logits
+    with torch.no_grad():
+        output = model(init_input)
+        logits = output.logits[:2]
+        logits = logits.detach().cpu().numpy()
+        predict_content = logits.argmax(axis=-1).flatten().tolist()
+    return logits
 
 print("Model Loaded")
